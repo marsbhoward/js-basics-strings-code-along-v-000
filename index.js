a2b2a5d5d32ba28542b5e9e09f39efcbd0e7bf15
Mars Howard <marsbhoward@gmail.com>
2019-03-12 01:29:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Mars'
 
-const welcomeMessage = 'Welcome to Flatbook ' + currentUser + '!' 
+const welcomeMessage = 'Welcome to Flatbook ' + currentUser + '!';
