818d2073c9348865454a93882e4aa1a7a136ae58
Mars Howard <marsbhoward@gmail.com>
2019-03-12 01:36:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Write your code in this file!
 const currentUser = 'Mars';
 
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
-
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase
