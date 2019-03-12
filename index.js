e05d6c96e1f7724c4e5c22c580bc027c6afa3516
Mars Howard <marsbhoward@gmail.com>
2019-03-12 01:42:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const currentUser = 'Mars';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting;
+const hello;
